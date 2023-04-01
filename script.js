ca6578655e26f4a207ec4a237279a45a51336239
Alex Ponciano <73028034+alexponciano2@users.noreply.github.com>
2023-04-01 14:23:40 -0300

    starting the script

---
+++ script.js
@@ -0,0 +1,7 @@
+let idade = document.getElementById('idade')
+let masc = document.getElementById('masc')
+let fem = document.getElementById('fem')
+let res = document.getElementById('res')
+
+let idadev = Number(idade.value)
+

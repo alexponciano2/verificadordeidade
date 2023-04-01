f9755bd76a84d5ac87b114aa9c4cda85a5c4c28c
Alex Ponciano <73028034+alexponciano2@users.noreply.github.com>
2023-04-01 15:39:26 -0300

    changed resolution of some pics

--- script.js
+++ script.js
@@ -1,7 +1,47 @@
-let idade = document.getElementById('idade')
-let masc = document.getElementById('masc')
-let fem = document.getElementById('fem')
-let res = document.getElementById('res')
+function verificar() {
+    let idade = document.getElementById('section#idade')    // idade informada no campo de números
+    const radioButtons = document.getElementsByName('genero');
+    let ver = document.getElementById('section#ver')        // botao VERIFICAR
+    let res = document.getElementById('div#res')
 
-let idadev = Number(idade.value)
 
+    // Adiciona um event listener para cada radio button
+    radioButtons.forEach(button => {
+        button.addEventListener('click', event => {
+            // Obtém o valor do radio button selecionado
+            const selectedValue = event.target.value;
+        });
+    });
+
+    let idadev = Number(idade.value)
+
+    let idadet = 2023 - idadev
+
+    if (idadet >= 1 && idade < 11) {
+        if (selectedValue == 'masculino') {
+            res.innerHTML = `Detectamos ${selectedValue} de ${idadet} anos.`;            
+        } else {
+            res.innerHTML = `Detectamos ${selectedValue} de ${idadet} anos.`;
+        }
+    } else if (idadet < 17) {
+        if (selectedValue == 'masculino') {
+            res.innerHTML = `Detectamos ${selectedValue} de ${idadet} anos.`;            
+        } else {
+            res.innerHTML = `Detectamos ${selectedValue} de ${idadet} anos.`;
+        }
+    } else if (idadet < 50) {
+        if (selectedValue == 'masculino') {
+            res.innerHTML = `Detectamos ${selectedValue} de ${idadet} anos.`;            
+        } else {
+            res.innerHTML = `Detectamos ${selectedValue} de ${idadet} anos.`;
+        }
+    } else if (idadet < 130) {
+        if (selectedValue == 'masculino') {
+            res.innerHTML = `Detectamos ${selectedValue} de ${idadet} anos.`;            
+        } else {
+            res.innerHTML = `Detectamos ${selectedValue} de ${idadet} anos.`;
+        }
+    } else {
+        res.innerHTML = `Idade inválida`
+    }
+}
